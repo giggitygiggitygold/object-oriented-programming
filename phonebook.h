53a8375a4234178928151c349fe1b6a5b35b5598
giggitygiggitygold <yxtagold.kirill@gmail.com>
2025-12-14 17:20:26 +0300

    phonebook.h

--- phonebook.h
+++ phonebook.h
@@ -2,19 +2,24 @@
 
 #include "contact.h"
 #include <vector>
+#include <string>
 
-class Phonebook{
-    private:
-        std::vector<Contact> contacts;
-    
-    public:
-        void addcontact(const Contact& c);
-        void removecontact(int index);
-        void editcontact(Phonebook& book);
+class Phonebook {
+private:
+    std::vector<Contact> contacts;
 
-        void save(const std::string& filename);
-        void load(const std::string& filename);
+public:
+    void addcontact(const Contact& c);
+    void removecontact(int index);
+    void editcontact();
 
-        void printall() const;
-        int size() const;
+    void save(const std::string& filename);
+    void load(const std::string& filename);
+
+    void printall() const;
+    void search(const std::string& query) const;
+    void sortByName();
+    void sortByEmail();
+
+    int size() const;
 };
